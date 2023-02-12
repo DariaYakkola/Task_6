9f9a8c85809e6f0487539ca44b7da27044f87f98
Daria Yakkola <yakkolabest@gmail.com>
2023-02-12 17:40:39 +0300

    Задача 6

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+if (numbers % 2 == 0 )
+{
+  Console.WriteLine(numbers + " - четное число");
+}
+else
+{
+  Console.WriteLine(numbers + " - нечетное число");
+}
